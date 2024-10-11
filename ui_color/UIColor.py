7ea04238cc232cc9ebd80d1fc0a4dd4c1f4e0a5a
Erik Eggeling <erik@minimallogic.com>
2024-10-11 10:55:46 +0200

    Layout in a grid and output json with indents

--- ui_color/UIColor.py
+++ ui_color/UIColor.py
@@ -41,11 +41,31 @@ class UIColor:
 			keys = key.split('.')
 			insert(nested_dict, keys, value)
 
+		nodeWidth = 160
+		nodeHeight = 130
+		spacing = 20
+		columns = 10  # Number of columns in the grid
+
+		nodes_list = []
+		current_column = 0
+		current_row = 0
+
 		for key in sorted(nested_dict.items()):
 			newOpr = c.create(baseCOMP, key[0])
-			TDF.arrangeNode(newOpr)
+			nodes_list.append(newOpr)
+			
+			newOpr.nodeX = current_column * (nodeWidth + spacing)
+			newOpr.nodeY = -current_row * (nodeHeight + spacing)  # Negative Y for grid going downward
+			
+			# Move to the next column, reset to new row if we've filled the current row
+			current_column += 1
+			if current_column >= columns:
+				current_column = 0
+				current_row += 1
+
 			first_page = newOpr.appendCustomPage(key[0])
 			newOpr.currentPage = first_page
+			
 			if type(key[1]) == tuple:
 				parname = f'{key[0]}'.capitalize()
 				par = first_page.appendRGB(parname, label=f'{key[0]}')
@@ -56,12 +76,13 @@ class UIColor:
 					parname = f"{item[0].replace('.', '')}".capitalize()
 					par = first_page.appendRGB(parname, label=f'{key[0]}.{item[0]}')
 					par.val = item[1]
-			continue
 
+			# Continue to the next item in the loop
+			continue
 
 	def Save(self):
 		with open(self.ownerComp.par.Path.val, 'w') as f:
-			f.write(json.dumps({color: ui.colors[color] for color in ui.colors}))
+			f.write(json.dumps({color: ui.colors[color] for color in ui.colors}, indent=2))
 		ui.messageBox('Saved', f'Saved {self.ownerComp.par.Path.val}')
 
 		return
@@ -92,11 +113,11 @@ def insert(d, keys, value):
 from collections.abc import MutableMapping
 
 def flatten_dict(d: MutableMapping, parent_key: str = '', sep: str ='.') -> MutableMapping:
-    items = []
-    for k, v in d.items():
-        new_key = parent_key + sep + k if parent_key else k
-        if isinstance(v, MutableMapping):
-            items.extend(flatten_dict(v, new_key, sep=sep).items())
-        else:
-            items.append((new_key, v))
-    return dict(items)
+	items = []
+	for k, v in d.items():
+		new_key = parent_key + sep + k if parent_key else k
+		if isinstance(v, MutableMapping):
+			items.extend(flatten_dict(v, new_key, sep=sep).items())
+		else:
+			items.append((new_key, v))
+	return dict(items)
